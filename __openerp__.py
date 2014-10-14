d6dd2935fced5c6ebaa8c8fc0c9b1c37981cdb10
Maeriden <daniele.bondi.dev@gmail.com>
2014-10-14 13:40:26 +0200

    Added custom accounts

--- __openerp__.py
+++ __openerp__.py
@@ -17,75 +17,78 @@ Customizzazione marketing per IES Consulting
     'author': 'Daniele Bondì @ Eikony',
     'website': 'http://www.eikony.com',
     'depends': [
-    	"base",
-    	"base_calendar",
-    	"web",
-    	"mail",
-    	"email_template",
-    	"crm",
-    	"sale",
-    	"sale_crm",
-    	"product",
-    	"account",
-    	"account_followup",
-    	"analytic",
-    	"account_analytic_analysis",
-    	"hr",
+        "base",
+        "base_calendar",
+        "web",
+        "mail",
+        "email_template",
+        "crm",
+        "sale",
+        "sale_crm",
+        "product",
+        "account",
+        "account_followup",
+        "analytic",
+        "account_analytic_analysis",
+        "hr",
     ],
-    
-    "data": [
-		#Security
-		"security/res.groups.xml",
-		"security/ir.rule.xml",
-		"security/ir.ui.menu.xml",
-		#"security/ir.model.access.csv", #Moved to .xml
-		"security/ir.model.access.xml",
-		
-		#Data
-		"data/crm.case.stage.xml", #Hide Lead column
-		
-		#"data/account.account.xml", TODO
-		"data/ir.cron_data.xml",
-		"data/res.users_data.xml",
-		"data/res.country.state_data.xml",
-		"data/res.alarm_data.xml",
-		"data/mail.template_data.xml",
-		"data/mail.subtype_data.xml",
-		"data/product.category_data.xml",
-		
-		"data/reminder_data.xml",
-		"data/defaults_data.xml",
 
-		#Modifications
-		"res.partner_view.xml",
-		"crm.lead_view.xml",
-		"sale_view.xml",
-		"analytic_view.xml",
-		
-		"review_view.xml",
-		"interview_view.xml",
-		"invoice_view.xml",
-		"defaults_view.xml",
-		"revenue.shares_view.xml",
-		"reminder_view.xml",
-		
-	#	"workflow/review_workflow.xml", #Incomplete
-		
-		#Wizards
-		"wizard/mail.invite.view.xml",
+    "data": [
+        #Security
+        "security/res.groups.xml",
+        "security/ir.rule.xml",
+        "security/ir.ui.menu.xml",
+        #"security/ir.model.access.csv", #Moved to .xml
+        "security/ir.model.access.xml",
+
+        #Data
+        "data/crm.case.stage.xml", #Hide Lead column
+
+        "data/account.account-base.xml",
+        "data/account.account-strategy.xml",
+        "data/account.account-s&o.xml",
+        "data/account.account-training.xml",
+        "data/ir.cron_data.xml",
+        "data/res.users_data.xml",
+        "data/res.country.state_data.xml",
+        "data/res.alarm_data.xml",
+        "data/mail.template_data.xml",
+        "data/mail.subtype_data.xml",
+        "data/product.category_data.xml",
+
+        "data/reminder_data.xml",
+        "data/defaults_data.xml",
+
+        #Modifications
+        "res.partner_view.xml",
+        "crm.lead_view.xml",
+        "sale_view.xml",
+        "analytic_view.xml",
+
+        "review_view.xml",
+        "interview_view.xml",
+        "invoice_view.xml",
+        "defaults_view.xml",
+        "revenue.shares_view.xml",
+        "reminder_view.xml",
+
+    #   "workflow/review_workflow.xml", #Incomplete
+
+        #Wizards
+        "wizard/mail.invite.view.xml",
     ],
-    
+
     "js": [
         "static/src/js/percentage.js",
     ],
     "qweb": [
-    	"static/src/xml/percentage.xml",
+        "static/src/xml/percentage.xml",
     ],
     "css": [
-    	"static/src/css/percentage.css",
-    	"static/src/css/table.css",
+        "static/src/css/percentage.css",
+        "static/src/css/table.css",
     ],
-    
+
     'demo': [],
     'test': [],
     'installable': True,
